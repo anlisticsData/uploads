e1675d36eff128f9f6d06bb9f383b61135504e2a
unknown <analisticsdataprojetos@gmail.com>
2024-05-11 10:39:37 -0300

    load

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'inforcomp/uploads',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e3d08ed0582152c8b18fd4b8bbdc3a3755c99136',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'inforcomp/uploads' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e3d08ed0582152c8b18fd4b8bbdc3a3755c99136',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
